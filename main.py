6b9395f56fa4469c0a3bf19d65dfe4d7afb7c2a9
jinhyeok15 <ekfrhd97@gmail.com>
2023-08-12 19:56:09 +0900

    변경

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import HTTPException
 
-import app
+from app import instance
 
 from schema.main import Img2imgRequestBody, Img2ImgResponse, Status, ImageDataMessageDto
 from service.main import create_progress, send_message
 
 
-@app.instance.post("/api/v1/img2img")
+@instance.post("/api/v1/img2img")
 def img2img_api(req: Img2imgRequestBody) -> Img2ImgResponse:
     is_success, response = create_progress(Status.SEND)
     if (not is_success):
