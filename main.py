63c1f032af5984756777d318a1872a1c484c9657
jinhyeok15 <ekfrhd97@gmail.com>
2023-08-12 20:59:06 +0900

    apps 삭제

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+
+from schema.main import Img2imgRequestBody, Img2ImgResponse, Status, ImageDataMessageDto
+from service.main import create_progress, send_message
 
 
 app = FastAPI()
@@ -15,3 +18,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.post("/api/v1/img2img")
+def img2img_api(req: Img2imgRequestBody) -> Img2ImgResponse:
+    is_success, response = create_progress(Status.SEND)
+    if (not is_success):
+        raise HTTPException(status_code=400, detail="progress등록에 실패하였습니다.")
+
+    send_message(ImageDataMessageDto(
+        **req.dict(),
+        id=response.id
+    ))
+
+    return response
