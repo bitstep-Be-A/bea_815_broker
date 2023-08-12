2b9fb176a06a1d1828c21a9fa29a73f44aa3ecb0
jinhyeok15 <ekfrhd97@gmail.com>
2023-08-12 20:49:10 +0900

    모듈 변경

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-from fastapi import HTTPException
-
-from app import instance
-
-from schema.main import Img2imgRequestBody, Img2ImgResponse, Status, ImageDataMessageDto
-from service.main import create_progress, send_message
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
-@instance.post("/api/v1/img2img")
-def img2img_api(req: Img2imgRequestBody) -> Img2ImgResponse:
-    is_success, response = create_progress(Status.SEND)
-    if (not is_success):
-        raise HTTPException(status_code=400, detail="progress등록에 실패하였습니다.")
+app = FastAPI()
 
-    send_message(ImageDataMessageDto(
-        **req.dict(),
-        id=response.id
-    ))
+origins = [
+    "*"
+]
 
-    return response
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
